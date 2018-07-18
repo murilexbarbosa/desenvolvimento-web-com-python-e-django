02c91cdc6d16e8aa170599096329b065797893d7
Murilo Barbosa <muka040789@hotmail.com>
2018-07-18 18:03:46 -0300

    criando home e base html

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo.core import views
 
 urlpatterns = [
+    path(r'', views.home),
     path('admin/', admin.site.urls),
 ]
