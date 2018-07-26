7aeda50642d53208b4ef62f7b7f4f81f3b3e7106
Murilo Barbosa <muka040789@hotmail.com>
2018-07-26 17:53:16 -0300

    add modulos tarefas.categoria

--- todo/tarefas/admin.py
+++ todo/tarefas/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import Categoria
 # Register your models here.
--- todo/tarefas/models.py
+++ todo/tarefas/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Categoria(models.Model):
-    nome =
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    descricao = models.TextField(verbose_name='Descrição')
