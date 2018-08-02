fe11350beca2d79ab4bdca3fa6a23406118350b8
Murilo Barbosa <muka040789@hotmail.com>
2018-08-02 17:04:42 -0300

    iniciando admin django

--- todo/tarefas/models.py
+++ todo/tarefas/models.py
@@ -5,3 +5,21 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
     descricao = models.TextField(verbose_name='Descrição')
+
+    def __str__(self):
+        return self.nome
+
+class Tarefa(models.Model):
+    PRIORIDADE_CHOICES = (
+        ('B', 'Baixa'),
+        ('M', 'Média'),
+        ('A', 'Alta'),
+    )
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    descricao = models.TextField(blank=True, verbose_name='Descrição')
+    data_final = models.DateField(verbose_name='Data Final')
+    prioridade = models.CharField(max_length=1, verbose_name='Prioridade', choices=PRIORIDADE_CHOICES)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categoria')
+
+    def __str__(self):
+        return self.nome
