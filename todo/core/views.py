e47c9df67ce0432574972f39e712e66ac65b303e
Murilo Barbosa <muka040789@hotmail.com>
2018-08-08 18:15:49 -0300

    listagem de tarefas

--- todo/core/views.py
+++ todo/core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 
+from todo.tarefas.models import Tarefa
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    tarefa = Tarefa.objects.all()
+    return render(request, 'core/index.html', {'tarefas': tarefa})
+
