c028dc2086fba0376995b487ece3615bf815e0c7
Murilo Barbosa <muka040789@hotmail.com>
2018-08-28 18:30:40 -0300

    implementação no cadastro de usuario

--- todo/accounts/forms.py
+++ todo/accounts/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
+    attrs = {
+        "type": "password"
+    }
+    password = forms.CharField(widget=forms.TextInput(attrs=attrs))
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'password')
