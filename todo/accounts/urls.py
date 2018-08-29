61770491008a7f62e9294f057433adf5b6d4a184
Murilo Barbosa <muka040789@hotmail.com>
2018-08-29 17:06:06 -0300

    ajustes na gravação de usuario e criação da tela de login

--- todo/accounts/urls.py
+++ todo/accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'add-user/', views.add_user, name='add_user'),
+    url(r'login-user/', views.login_user, name='login_user'),
 ]
--- todo/accounts/views.py
+++ todo/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 from .forms import UserForm
 # Create your views here.
@@ -8,9 +10,26 @@ def add_user(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
+            u = form.save()
+            u.set_password(u.password)
+            u.save()
             return HttpResponse('Usuário gravado!')
     else:
         form = UserForm()
     return render(request, 'accounts/add_user.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('core')
+        else:
+            messages.error(request, 'Usuário ou senha inválido.')
+    else:
+        render(request, 'accounts/user_login.html')
 
